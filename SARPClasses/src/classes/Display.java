17a93325c6f5c2b18a3020ca57a5a979514fd9d4
EQUIPO\Usuario <Juan_carriquiry@hotmail.com>
2016-09-17 16:21:50 -0300

    Creacion de geters y seters clase Display
    Creacion de geters y seters clase Display

--- SARPClasses/src/classes/Display.java
+++ SARPClasses/src/classes/Display.java
@@ -1,5 +1,38 @@
 package classes;
 
-public class Display {
+import java.util.Date;
+import java.util.List;
 
+public class Display {
+	private String rutaArchivo;
+	private Date fechaCreacion;
+	private Date fechaUltModificaion;
+
+	public Display(){
+	}
+
+	public Display(String rutaArchivo ){
+		this.fechaCreacion = new Date();
+		this.fechaUltModificaion= new Date();
+		this.rutaArchivo=rutaArchivo;
+	}
+
+	public String getRutaArchivo() {
+		return rutaArchivo;
+	}
+
+	public void setRutaArchivo(String rutaArchivo) {
+		this.rutaArchivo = rutaArchivo;
+	}
+
+	public Date getFechaUltModificaion() {
+		return fechaUltModificaion;
+	}
+
+	public void setFechaUltModificaion(Date fechaUltModificaion) {
+		this.fechaUltModificaion = fechaUltModificaion;
+	}
+	
+	
 }
+
